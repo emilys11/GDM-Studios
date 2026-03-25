485d3721afba063ee9427d1490d2b006f8770722
Olivia <oliviacardillo@yahoo.ca>
2026-03-25 00:17:58 -0400

    Added music to planet 1

--- Assets/Rhythm (Emily)/Scripts/EnemyHealth.cs
+++ Assets/Rhythm (Emily)/Scripts/EnemyHealth.cs
@@ -32,7 +32,7 @@ public class EnemyHealth : MonoBehaviour
 
     void TakeDamage()
     {
-        currentHP -= 50;
+        currentHP -= 50000;
         hpBar.value = (float)currentHP / maxHP;
 
         if (currentHP <= 0)
