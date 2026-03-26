caa9f4540a7b5d88bf1d85b8887048f6b0b99790
Taym <taym.wathek1@gmail.com>
2026-03-25 20:54:55 -0400

    Popula Modif + Ending Modif

---
+++ Assets/Exploration/Transition/EndSceneLoader.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+
+        SceneManager.LoadScene("Menu");
+
+    }
+
+
+
+
+
+}
